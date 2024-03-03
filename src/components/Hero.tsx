9cab87b5e2e50bc13fb7832f826f28116d8402cd
azizul-orvee <azizulhakim@Rakibuls-MacBook-Pro.local>
2024-03-04 00:57:20 +0600

    time

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,12 +13,13 @@ export function Hero() {
             Mobile Tyre <br />{" "}
             Fitting
           </h1>
-          <p className="pt-6 text-base w-auto sm:w-10/12 md:w-10/12 dark:text-neutral-400">
-            Plantilla de aplicación de página única (SPA) desarrollada con
-            Next.js, TypeScript y TailwindCSS. Personalizable y algunas
-            secciones para agregar información importante sobre un producto o un
-            servicio.
-          </p>
+          <h1 className="p-1 mt-6 text-lg font-semibold shadow-xl italic w-auto sm:w-10/12 md:w-10/12 bg-gray-100">
+              45-60 mins response time almost anywhere in the UK
+              </h1>
+          <h1 className="pt-6 text-xl font-medium w-auto sm:w-10/12 md:w-10/12">
+          Stumbled upon us during your Google search? Great news! you are now moments away from our mobile tyre fitting service available. <br />{" "} <br />{" "} 
+          Whether you are at home, work, or stranded by the roadside, our solution is designed for your utmost convenience.
+          </h1>
           <div className="flex flex-auto pt-10 gap-2 min-w-[350px] justify-center sm:justify-start">
             <div>
            <Link
